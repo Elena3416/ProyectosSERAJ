18bf2cb30d00bce71e0d5085975f5397d769124b
Elena3416 <lauelena_14@hotmail.com>
2020-10-17 00:25:20 -0500

    ImprimirOperacionesJavaScript

---
+++ Javascript/Tareas/JS/DivisionNumeros.js
@@ -0,0 +1,14 @@
+function DivisionNumeros(){
+
+    let num1 = 100;
+    let num2 = 50;
+    let division = 0;
+
+    division = num1 / num2;
+
+    document.write (num1 / num2);
+
+    console.log(num1 / num2);
+
+    alert("La division es:" + " " + division);
+}
